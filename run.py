1877d69a070841a770a4fdcf84c41fa4bcba6aaf
Treesss <912248666@qq.com>
2024-05-08 00:56:12 +0800

    [ADD]增加接口

--- run.py
+++ run.py
@@ -6,10 +6,8 @@ from wxcloudrun import db
 
 # 启动Flask Web服务
 # 检查命令行参数
-if len(sys.argv) > 1 and sys.argv[3] == 'init_db':
-    with app.app_context():
-        print("xxxxxxxxxxxxx")
-        db.create_all()
 
 if __name__ == '__main__':
     app.run(host=sys.argv[1], port=sys.argv[2])
+    with app.app_context():
+        db.create_all()
